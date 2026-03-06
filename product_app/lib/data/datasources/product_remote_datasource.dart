21fb85cfb64cf60c7f38d88fcc4e7b49ecf1bc0f
Daniel <daniel.junior137@hotmail.com>
2026-03-05 21:41:38 -0300

    Feat: adicionando arquivos estruturais necessários para o correto funcionamento da aplicação mobile.

---
+++ product_app/lib/data/datasources/product_remote_datasource.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:product_app/data/models/product_model.dart';
+
+class ProductRemoteDatasource {
+  final http.Client client;
+  ProductRemoteDatasource(this.client);
+
+  Future<List<ProductModel>> getProducts() async {
+    final response = await client.get(
+      Uri.parse("https://fakestoreapi.com/products"),
+    );
+    final List data = jsonDecode(response.body);
+    return data
+        .map((json) => ProductModel.fromJson(json))
+        .toList();
+  }
+}
---
+++ product_app/lib/domain/repositories/product_repository.dart
@@ -0,0 +1,6 @@
+
+import 'package:product_app/domain/entities/product.dart';
+
+abstract class ProductRepository {
+Future < List < Product > > getProducts () ;
+}
